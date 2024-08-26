e15e5e655a7a8fbf894d3affa1805c7f503da7d9
sohanxsahoo <sohanxsahoo@gmail.com>
2024-08-26 13:58:31 +0530

    env update

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,7 +14,7 @@ export async function middleware(request: NextRequest) {
     )) {
         return NextResponse.redirect(new URL('/dashboard', request.url));
     }
-    // return NextResponse.redirect(new URL('/home', request.url));
+    return NextResponse.redirect(new URL('/home', request.url));
 }
 
 
